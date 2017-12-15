06dd45c2b617b990c4dbb4c87bc0060dbc7550c2
Julius Freiny <julius.freiny@rwth-aachen.de>
2017-12-15 17:46:31 +0100

    added fibonacci for another testing

--- src/ai/ChezzAI.java
+++ src/ai/ChezzAI.java
@@ -2,9 +2,20 @@ package ai;
 
 public class ChezzAI {
 
-	String hallo = "Hallo";
+	static String hallo = "Hallo";
 	public static void main(String[] args) {
 		System.out.println(hallo);
+		System.out.println(fib(10));
+	}
+	
+	private static int fib(int x) {
+		if(x <= 0) {
+			return 0;
+		}else if(x < 2) {
+			return 1;
+		} else {
+			return fib(x-1)+fib(x-2);
+		}
 	}
 
 }
