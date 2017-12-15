b1f3130c67fa893e8be44c4323d436b48c51bd86
SargentWolfer <SargentWolfer@gmail.com>
2017-12-15 20:59:31 +0100

    try to merge

--- src/logic/Logic.java
+++ src/logic/Logic.java
@@ -1,8 +1,40 @@
 package logic;
 
 public class Logic {
-	
-	public String[] getAllPossibleMoves(String[] board) {
+	static byte[][] positionBoard = new byte[8][8];
+	public static String[] getAllPossibleMoves(String[] board) {
+		for(int i = 0; i<= 7; i++) {
+			for(int a = 0; a<=7; a++) {
+				for(int z = 1; z<= board.length; z++){
+
+					if((int)board[z].charAt(3) == 49 + a && (board[z].charAt(2) - 'a')-a == 0 ){
+						if(board[z].charAt(1) == 'b') {
+							positionBoard[i][a] = 1;
+							break;
+						} else {
+							positionBoard[i][a] = 2;
+							break;
+						}
+
+						} else {
+
+							positionBoard[i][a] = 0;
+							break;
+						}
+				}
+				System.out.println(positionBoard[i][a]);
+
+			}
+
+		}
+
+
+
+
+
+
+
+
 		return null;
-	}
+	};
 }
