d98bd63fc9e88ea43e16090594ffca1db4b8349f
Julius Freiny <julius.freiny@rwth-aachen.de>
2017-12-15 20:23:34 +0100

    basic interaction

--- src/ai/ChezzAI.java
+++ src/ai/ChezzAI.java
@@ -1,5 +1,9 @@
 package ai;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Random;
 import java.util.Scanner;
 
 import logic.Logic;
@@ -12,11 +16,18 @@ public class ChezzAI {
 			"pwa2","pwb2","pwc2","pwd2","pwe2","pwf2","pwg2","pwh2",
 			"rwa1","nwb1","bwc1","qwd1","kwe1","bwf1","nwg1","rwh1",
 			};
+	static String[] commentary;
+	
 	static Scanner scanner;
-
-	public static void main(String[] args) {
+	
+	static Random random;
+	
+	public static void main(String[] args) throws FileNotFoundException {
 		//String[] moves = Logic.getAllPossibleMoves(initialBoard);
 		scanner = new Scanner(System.in);
+		random = new Random();
+		
+		loadCommentary("res/commentary_de.txt");
 		
 		System.out.println("Hallo,\ndies ist ein simpler Schachcomputer. Möchtest du ein Spiel spielen?");
 		switch(scanner.nextLine().toLowerCase()) {
@@ -31,23 +42,66 @@ public class ChezzAI {
 	}
 	
 	static void startGame() {
-		printBoard();
-		System.out.print("Bitte gib deinen Zug ein: ");
-		boolean correctMove = false;
-		String[] move = null;
-		while(!correctMove) {
-			correctMove = true;
-			move = scanner.nextLine().split(" ");
-			if(move.length != 4) {
-				System.err.println("[Error] Falsches Zugformat: <Figur> <Startposition> nach <Endposition>");
-				correctMove = false;
+		boolean winner = false;
+		while(!winner) {
+			// User am Zug
+			printBoard();
+			System.out.print("Bitte gib deinen Zug ein: ");
+			boolean correctMove = false;
+			String[] move = null;
+			while(!correctMove) {
+				correctMove = true;
+				move = scanner.nextLine().split(" ");
+				if(move.length != 4) {
+					System.err.println("[Error] Falsches Zugformat: <Figur> <Startposition> nach <Endposition>");
+					correctMove = false;
+				}
+				if(!processMove(move)) {
+					System.out.println("[Error] Du hast keinen gültigen Zug eingegeben");
+					correctMove = false;
+				}
 			}
-		}
-		processMove(move);
+			
+			// Computer am Zug
+			printBoard();
+			comment();
+			winner = true;
+		}		
 	}
 	
-	static void processMove(String[] move) {
-		
+	static void comment() {
+		System.out.println(commentary[random.nextInt(100) % commentary.length]);
+	}
+	
+	static void loadCommentary(String filename) throws FileNotFoundException {
+		Scanner in = new Scanner(new FileReader(filename));
+		StringBuilder sb = new StringBuilder();
+		while(in.hasNext()) {
+			sb.append(in.nextLine());
+			sb.append("___");
+		}
+		in.close();
+		commentary = sb.toString().split("___");
+	}
+	
+	static boolean processMove(String[] move) {
+		String p = " ";
+		switch(move[0].toLowerCase()) {
+		case "bauer": p = "p"; break;
+		case "turm": p = "r"; break;
+		case "läufer": p = "b"; break;
+		case "springer": p = "n"; break;
+		case "dame": p = "q"; break;
+		case "könig": p = "k"; break;
+		}
+		boolean successfulMove = false;
+		for(int i = 0; i < board.length; i++) {
+			if(board[i].equals((p+"w"+move[1]))) {
+				board[i] = p+"w"+move[3];
+				successfulMove = true;
+			}
+		}
+		return successfulMove;
 	}
 	
 	static void printBoard() {
@@ -59,7 +113,7 @@ public class ChezzAI {
 			output += "#";
 				for(int column = 0; column < 8; column++) {
 					if(smallRow == 1) {
-						output += "  "+charBoard[row][column]+"  #";
+						output += "  "+charBoard[7 - row][column]+"  #";
 					} else if(smallRow == 3) {
 						output += "######";
 					}else {
